279d597283bdebd3b2c6e551630c39f86b5c0110
Luan de Gregori <luan.gregori@compasso.com.br>
2021-04-09 11:17:44 -0300

    feat: add router to healthcheck

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,15 @@
 import * as express from 'express';
 import TesteController from './controllers/testeController';
 
-// const validator = require('../validators/validator');
-
 const router = express.Router();
 
 const testeController = new TesteController();
 const contextPath = process.env.CONTEXT_PATH_ENV;
+
+// Status do backend
+router.get(`${contextPath}/healthcheck`, (req, res) => {
+  return res.status(200).json({ status: 'UP' });
+});
 
 router.get(
   `${contextPath}/teste`, 
