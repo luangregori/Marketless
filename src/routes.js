5cf4989ae6d1ad1d9a841ddedab5c0ecf00c9429
Luan de Gregori <luan.gregori@compasso.com.br>
2021-04-12 11:02:08 -0300

    feat: add router to healthcheck

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,11 @@ const routes = new Router();
 
 const contextPath = process.env.CONTEXT_PATH_ENV;
 
+// Status do backend
+routes.get('/healthcheck', (req, res) => {
+  return res.status(200).json({ status: 'UP' });
+});
+
 routes.post( `${contextPath}/users`, UserController.store);
 
 routes.post( `${contextPath}/login`, SessionController.store);
